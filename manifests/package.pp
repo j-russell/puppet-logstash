0f3a9204a031108fb6df99b81aadf235840e6db5
Josh Russell <jrussell@hubspot.com>
2013-04-26 18:10:36 -0400

    can't reassign variable

--- manifests/package.pp
+++ manifests/package.pp
@@ -6,36 +6,38 @@ class logstash::package (
   $logstash_baseurl      = $::logstash::config::logstash_baseurl) {
   Class['::logstash::config'] -> Class['::logstash::package']
 
-  if ! $logstash_jar {
-    $logstash_jar = sprintf('%s-%s-%s', 'logstash', $logstash_version, 'flatjar.jar')
+  if !$logstash_jar {
+    $logstash_jar_file = sprintf('%s-%s-%s', 'logstash', $logstash_version, 'flatjar.jar')
+  } else {
+    $logstash_jar_file = $logstash_jar
   }
 
-  $jar          = "${logstash_home}/${logstash_jar}"
+  $jar = "${logstash_home}/${logstash_jar_file}"
 
   if $logstash_jar_provider == 'package' {
     package { 'logstash':
       ensure => 'latest',
     }
   } elsif $logstash_jar_provider == 'puppet' {
-    file { "${logstash_home}/${logstash_jar}":
+    file { $jar:
       ensure => present,
-      source => "puppet:///modules/logstash/${logstash_jar}",
+      source => "puppet:///modules/logstash/${logstash_jar_file}",
     }
   }
 
-  $logstash_url = "${logstash_baseurl}/${logstash_jar}"
+  $logstash_url = "${logstash_baseurl}/${logstash_jar_file}"
 
-  exec { "curl -o ${logstash_home}/${logstash_jar} ${logstash_url}":
+  exec { "curl -o ${jar} ${logstash_url}":
     timeout => 0,
     cwd     => '/tmp',
-    creates => "${logstash_home}/${logstash_jar}",
+    creates => $jar,
     path    => [
       '/usr/bin',
       '/usr/sbin'],
   }
 
   if $logstash_jar_provider == 'external' {
-    notify { "It's up to you to provde ${logstash_jar}":
+    notify { "It's up to you to provide ${logstash_jar_file}":
     }
   }
 }
