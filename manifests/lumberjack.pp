c186bf7163ad3a08ba18b95ea42905fe52f9bb03
Josh Russell <jrussell@hubspot.com>
2013-05-15 14:19:32 -0400

    support for overriding global host|port|ssl-ca-path in config file

--- manifests/lumberjack.pp
+++ manifests/lumberjack.pp
@@ -1,7 +1,10 @@
 define logstash::lumberjack (
   $file,
+  $host = undef,
+  $port = undef,
+  $ssl_ca_path = undef,
   $window_size = undef,
-  $fields      = {}
+  $fields      = undef,
 ) {
   if !defined(Class['::logstash::lumberjack::package']) {
     class { '::logstash::lumberjack::package': }
@@ -13,8 +16,38 @@ define logstash::lumberjack (
 
   Class['::logstash::lumberjack::package'] -> Class['::logstash::lumberjack::config']
 
+  if $host {
+    $lumberjack_host = "--host ${host} "
+  } else {
+    $lumberjack_host = ''
+  }
+
+  if $port {
+    $lumberjack_port = "--port ${port} "
+  } else {
+    $lumberjack_port = ''
+  }
+
+  if $ssl_ca_path {
+    $lumberjack_ssl_ca_path = "--ssl-ca-path ${ssl_ca_path} "
+  } else {
+    $lumberjack_ssl_ca_path = ''
+  }
+
+  if $window_size {
+    $lumberjack_window_size = "--window-size ${window_size} "
+  } else {
+    $lumberjack_window_size = ''
+  }
+
+  if $fields {
+    $lumberjack_fields = inline_template('<% @fields.sort.map do |name,value|-%>--field <%=name%>=<%=value%> <% end -%>')
+  } else {
+    $lumberjack_fields = ''
+  }
+
   concat::fragment { "lumberjack_${name}":
     target  => '/etc/lumberjack/lumberjack.conf',
-    content => $file,
+    content => "${lumberjack_host}${lumberjack_port}${lumberjack_ssl_ca_path}${lumberjack_window_size}${lumberjack_fields}${file}",
   }
 }
