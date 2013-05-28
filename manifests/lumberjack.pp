f2b6b7e4229deb7babce2f98ae52f4d5e5e797fc
Josh Russell <jrussell@hubspot.com>
2013-05-28 16:28:03 -0400

    quoting breaks the file path

--- manifests/lumberjack.pp
+++ manifests/lumberjack.pp
@@ -52,6 +52,6 @@ define logstash::lumberjack (
 
   concat::fragment { "lumberjack_${name}":
     target  => '/etc/lumberjack/lumberjack.conf',
-    content => "${lumberjack_host}${lumberjack_port}${lumberjack_ssl_ca_path}${lumberjack_window_size}${lumberjack_fields}\"${file}\"\n",
+    content => "${lumberjack_host}${lumberjack_port}${lumberjack_ssl_ca_path}${lumberjack_window_size}${lumberjack_fields}${file}\n",
   }
 }
